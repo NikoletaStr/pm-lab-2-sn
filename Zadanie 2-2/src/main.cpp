28ae228dd12638f7bd0c265a148d0f264b39d2cb
Nikoleta Strózyk <nikoleta.strozyk@student.wat.edu.pl>
2020-11-30 10:37:24 +0100

    Rozpoczętozadanie 2.3–IRQ

--- Zadanie 2-2/src/main.cpp
+++ Zadanie 2-2/src/main.cpp
@@ -1,27 +1,5 @@
-#include <avr/io.h>
-
-uint8_t button;
-bool state = true;
-
-void togglePinD13(bool *state) {
-  PORTB = (*state << 5);
-  *state = !(state);
-}
-
-void delay() {
-  for (uint32_t j = 0x1FFFF; 1 > 0; j--)
-    __asm__ __volatile__("nop");
-}
-
+#include <Arduino.h>
 int main() {
-  DDRB &= !(1 << 0);
-  DDRB |= (1 << 5);
-  while (1) {
-    button = (PINB & (1 << PINB0));
-    if (button == 0) {
-      togglePinD13(&state);
-    }
-    else
-      delay();
-  }
+  init();
+  while (1) { /*code*/ }
 }
